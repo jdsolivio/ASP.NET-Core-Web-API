578497ddca9ba99d7b1c3457e7ec568319c18ec2
Jumryl <jdsolivio.devops@gmail.com>
2024-03-21 09:43:16 +0800

    Successful

--- Repository/IAccount.cs
+++ Repository/IAccount.cs
@@ -1,4 +1,5 @@
-﻿using static WebOopPrac_Api.Models.ServiceResponse;
+﻿using WebOopPrac_Api.Models;
+using static WebOopPrac_Api.Models.ServiceResponse;
 using static WebOopPrac_Api.Models.TodoModel;
 
 namespace WebOopPrac_Api.Repository
@@ -6,6 +7,6 @@ namespace WebOopPrac_Api.Repository
     public interface IAccount
     {
         Task<ServiceResponseModel<IEnumerable<dynamic>>> CredInsert(string title, string description, bool IsCompleted);
-
+        Task<IEnumerable<TodoModel>> GetTodoModel();
     }
 }
